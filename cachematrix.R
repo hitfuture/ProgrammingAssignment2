0999a4810b5a073b35fbe579a63acbcad80045ad
HITFuture <brett.r.taylor@icloud.com>
2015-01-21 06:37:36 -0700

    It looks like all of the tests pass

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,26 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix() will create a special object that will enable the capture of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                #look up variables in the next environment (<<- vs. <-) 
+               # x <- y  #Let's break it - why does it break? 
+                #The is due to Lexical scoping. The set function environment is different than the makeCacheMatrix()
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(invertedMatrix) inv <<- invertedMatrix
+        getInverse <- function() inv
+        #This list is an object that contains the behaviors defined
+        #in the functions above
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+        
 }
 
 
@@ -12,4 +28,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        #Assume that this matrix can be inverted
+        inv <- x$getInverse()
+        if(!is.null(inv)) { #If the inverse is cached -return it without processing
+                message("getting cached matrix")
+                return(inv)
+        }
+        #We know the inverse is not cached - so recompute
+        data <- x$get()
+        inv <- solve(data, ...)
+        #Cache the data
+        x$setInverse(inv)
+        #Return the inverse matrix
+        inv
 }
